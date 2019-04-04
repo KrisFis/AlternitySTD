d4ef854de4c7ab3f92dea51f9d148908b3e08bca
KrisFis <krisfis@email.cz>
2019-04-04 17:59:19 +0200

    Configured VS studio

---
+++ Source/Private/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define print(text) std::cout << text << std::endl
+#define printWithArg(text, var) std::cout << text << var << std::endl
+#define read(var) std::cin >> var
+#define wait() system("pause")
+
+int main(void)
+{
+
+	return EXIT_SUCCESS;
+}
---
+++ Source/Public/Components/IntTypes.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int int16;
+typedef unsigned short int uint16;
+typedef long int32;
+typedef unsigned long uint32;
